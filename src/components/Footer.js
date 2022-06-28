7f95830e26b2b20e4ea97a700f362d977c8d7e90
Nirav <niravdamor2002@gmail.com>
2022-06-28 17:46:24 +0530

    UI setup

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css';
+import { Container } from "react-bootstrap";
+function Layout(props) {
+    return (
+        <>
+            <Container fluid className="text-center"> 
+                Robo-Nav Labs &copy; 2022
+            </Container>
+        </>
+    )
+}
+
+export default Layout;
---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from './Navigation';
+import Footer from './Footer';
+function Layout(props)
+{
+    return (
+        <>
+            <Navigation />
+            <main>
+                {props.children}
+            </main>
+            <Footer/>
+        </>
+    )
+}
+
+export default Layout;
